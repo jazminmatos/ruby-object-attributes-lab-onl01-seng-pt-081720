b4f29fb7face9ec5f263797060f7c45767ab77e3
Jazmin Matos <jmatos1993@gmail.com>
2020-09-02 03:20:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,16 @@ class Dog
   end
 
 end
+
+
+  #def initialize(dog_breed) #initialize
+   # @breed = dog_breed
+  #end
+
+#  def breed=(dog_breed) #setter method
+  #  @breed = dog_breed
+#  end
+
+#  def breed #getter method
+ #   @breed
+  #end
